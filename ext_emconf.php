8c49987fb84115d999f5f87247471ec549ac83ce
Łukasz Uznański <l.uznanski@macopedia.com>
2024-02-22 15:11:49 +0100

    init v12 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.4.0-11.5.99'
+            'typo3' => '11.4.0-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
